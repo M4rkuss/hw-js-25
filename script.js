1e53e070279cdc9cf4b5fc6726f97dea149083e2
m4rkuss <markbu07@gmail.com>
2022-05-24 00:06:48 +0300

    [+] did 2nd task

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
+//1)
 function completeMessage(msg) {
   window.document.getElementById('completeDownload').innerHTML += msg + "<br/>"
 }
 
 function errorMessage(msg) {
   window.document.getElementById('errorDownload').innerHTML += msg + "<br/>"
+}
+
+//2)
+window.onbeforeunload = function (e) {
+  if (document.getElementById('text1').value.length > 0 || document.getElementById('text2').value.length > 0) {
+    let msg = "Text not saved";
+    e.returnValue = msg;
+    return msg;
+  }
+  return  null
 }
