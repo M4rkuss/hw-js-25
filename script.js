d5a104bee37ecd2fcb80cc9a0ca4ae9e3057832c
m4rkuss <markbu07@gmail.com>
2022-05-23 23:59:13 +0300

    [+] did 1st task

---
+++ script.js
@@ -0,0 +1,7 @@
+function completeMessage(msg) {
+  window.document.getElementById('completeDownload').innerHTML += msg + "<br/>"
+}
+
+function errorMessage(msg) {
+  window.document.getElementById('errorDownload').innerHTML += msg + "<br/>"
+}
